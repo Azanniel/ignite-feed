2f10475ab83472a6d87f63dc47a91c34b7a2e96c
azanniel <leo.azannielttt@gmail.com>
2022-12-21 16:12:37 -0400

    feat: added dynamic posts

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,56 @@ import { Sidebar } from './components/Siderbar';
 import './global.css';
 import styles from './App.module.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/azanniel.png',
+      name: 'Leandro Azanniel',
+      role: 'Programador'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-12-20 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/Moises-Bruce.png',
+      name: 'Moisés Bruce',
+      role: 'Programador'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2022-12-21 16:10:00')
+  },
+];
+
 export function App() {
   return (
     <div>
       <Header />
-  
+
       <div className={styles.wrapper}>
         <Sidebar />
 
         <main>
-          <Post />
-          
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
