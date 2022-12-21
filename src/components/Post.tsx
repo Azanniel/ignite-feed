91ca9fc34bd5c01779f107c7cef701ba7a60595f
azanniel <leo.azannielttt@gmail.com>
2022-12-20 21:21:53 -0400

    feat: added footer to form and learning about visibility in css

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -33,6 +33,18 @@ export function Post() {
           <a href="#">#rocketseat</a>{' '}
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder='Deixei um comentário'
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
